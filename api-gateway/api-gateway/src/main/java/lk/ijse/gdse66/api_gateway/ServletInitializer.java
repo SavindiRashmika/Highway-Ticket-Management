df5717f4485d9d6f16def0a8370c6b8c01d71c04
SavindiRashmika <rashmikawadanambi>
2024-06-24 22:27:26 +0530

    api getway added

--- api-gateway/api-gateway/src/main/java/lk/ijse/gdse66/api_gateway/ServletInitializer.java
+++ api-gateway/api-gateway/src/main/java/lk/ijse/gdse66/api_gateway/ServletInitializer.java
@@ -1,2 +1,12 @@
-package lk.ijse.gdse66.api_gateway;public class ServletInitializer {
+package lk.ijse.gdse66.api_gateway;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiGatewayApplication.class);
+    }
 }
